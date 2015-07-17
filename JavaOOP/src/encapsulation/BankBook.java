fa143e1523c698be7a034795f6fd73c9b13f8a7d
평일반 <@ITBANK10-PC>
2015-07-17 15:19:30 +0900

    2015.07.17 자바 10일차

--- JavaOOP/src/encapsulation/BankBook.java
+++ JavaOOP/src/encapsulation/BankBook.java
@@ -1,5 +1,7 @@
 package encapsulation;
 
+import abstraction.Account;
+
 /*
  객체지향 프로그래밍에서 클래스는 현실 세계를 그대로 카피하려고 한다.
  그 대상이 구체적 물질이든 개념적 추상이든 그대로 재현하려고 해야한다.
@@ -10,7 +12,9 @@ package encapsulation;
  3. 소유자 이름 => 생성자에 스캐너로 입력받는값 name
  4. 날짜 / 예금 / 출금 / 잔액 => 돈 => 스캐너로 입력받는 값 숫자타입 int => money
  5. 비밀번호 => 스캐너로 입력받는값, 숫자타입 => pass
- 
+ 인터페이스 
+ 1. withdraw(int money) : void
+ 2. deposit(int money) : void
  
  [출력]
  ==============
@@ -19,16 +23,96 @@ package encapsulation;
  이름 : 홍길동
  잔액 : 1000원
  ==============
+ BankBook이라는 클래스 라이브러리를
+ NDriver에서 다운받아서 사용하세요.
+ 비번은 돈 먼저 입금하세요.
  * */
-public class BankBook {
+
+
+public class BankBook implements Account{
+
+	//상수 : final을 붙여서 절대 불변의 값을 만든다.
+	//스태틱 : final을 빼버리면 절대 불변의 값이 아니라 바꿀 수 있지만, 고정된 값 스태틱 값이 된다.
+	//스태틱 = 클래스 변수
+	//BankBook b = new BankBook();
+	//b.bank --> 인변이 값을 호출한 것
+	//BankBook.bank --> 클래스변수 호출하는 모양
+	//위치 : 필드--> {인변,상수,클래스변수} < 멤변  
+	
+/*===== 멤버필드 =====*/	
+	private static String BANK;
+	private int bankbookNo;
+	private String name;
+	private int money;
+	private int pass;
+	private String msg;
+	//유효성체크 결과를 알려줘서 고객이 만약 실수로 그렇게 입력했다면 공지해주는 기능
+	
+	public BankBook(String name) {
+		this.BANK="아이티뱅크";
+		this.bankbookNo = (int) ((Math.random()*99999)+10000);
+		this.name=name;
+	}
+	
+	//개발자는 setter를 쓸지, 생성자로 끝낼지를 고민
+	//getter setter 단축키 : ART + SHIFT +S
+	
+	
 	@Override
 	public String toString() {
 		return "==============\n"
-				+"[아이티뱅크]\n"
-				+"계좌번호 : " + 12345
-				+"\n이름 : " + 홍길동
-				+"\n잔액 : " + 1000 +"원"
+				+"["+ BANK +"]\n"
+				+"계좌번호 : " + bankbookNo
+				+"\n이름 : " + name
+				+"\n잔액 : " + money +"원"
 				+"\n==============";
+	}
+
+	public int getBankbookNo() {
+		return bankbookNo;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public int getMoney() {
+		return money;
+	}
+
+	public void setMoney(int money) {
+		this.money = money;
+	}
+	public void setPass(int pass) {
+		this.pass = pass;
+	}
+	public int getPass() {
+		return pass;
+	}
+
+	@Override
+	public void withdraw(int money) {
+		//파라미터 값이 정상 값이 아닌 상태를 추적해서 필터링하는 로직이 필요해짐
+		//==> 유효성 체크
+		if (money <= 0) {
+			msg = "출금액은 0보다 커야합니다.";
+		} else if (this.money < money) {
+			msg = "출금액이 잔액보다 큽니다. ";
+		} else{
+		this.money -= money; // 누적된 값에서 마이너스
+		}
+		
+	}
+
+	@Override
+	public void deposit(int money) {
+		if (money <= 0) {
+			msg = "출금액은 0보다 커야합니다.";
+		} else {
+			this.money += money;
+		}
+
+		
 	}
 
 }
